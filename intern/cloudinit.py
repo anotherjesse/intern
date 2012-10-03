e64536db12da3b851884ff84b9ec7e84f05c9677
Jesse Andrews <anotherjesse@gmail.com>
2012-10-03 13:30:32 -0700

    fixes None options - uses get instead of in

--- intern/cloudinit.py
+++ intern/cloudinit.py
@@ -2,20 +2,20 @@
 
 def cloudconfig(options):
     lines = ['#cloud-config']
-    if 'ssh_key' in options:
-        lines.append('ssh_authorized_keys:\n - %s' % options['ssh_key'])
-    if 'packages' in options:
+    if options.get('apt_proxy'):
+        lines.append('apt_proxy: %s' % options['apt_proxy'])
+    if options.get('hostname'):
+        lines.append('hostname: %s' % options['hostname'])
+    if options.get('packages'):
         lines.append('packages:')
         for p in options['packages']:
             lines.append(' - %s' % p)
-    if 'apt_proxy' in options:
-        lines.append('apt_proxy: %s' % options['apt_proxy'])
-    if 'hostname' in options:
-        lines.append('hostname: %s' % options['hostname'])
     if options.get('script', None):
         lines.append('runcmd:\n - |')
         for l in options['script'].split('\n'):
             lines.append('  %s' % l)
+    if options.get('ssh_key'):
+        lines.append('ssh_authorized_keys:\n - %s' % options['ssh_key'])
     return '\n'.join(lines)
 
 
