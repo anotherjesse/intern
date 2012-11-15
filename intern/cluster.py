5d436d8eda8053bf84261bea7f2f2f0765d9f0b9
Jesse Andrews <anotherjesse@gmail.com>
2012-11-15 00:38:40 -0800

    support creation of multiple VMs based on cluster definition

--- intern/cluster.py
+++ intern/cluster.py
@@ -1,7 +1,11 @@
+from intern import cloud
+
+
 class Cluster(object):
     """a collection of VMs"""
 
-    def __init__(self, vms=None):
+    def __init__(self, name=None, vms=None):
+        self.name = name
         if vms:
             self.vms = vms
         else:
@@ -32,3 +36,40 @@ class Cluster(object):
         for vm in self:
             yield vm.get(path)
 
+    def build(self, name, spec):
+        """initialize a named cluster with a spec
+
+        spec is a dictionary with keys of group(s) and value of qty"""
+        self.name = name
+
+        existing = [v for v in cloud.find() if v.server.metadata.get('cluster', None) == self.name]
+
+        existing_names = [e.name for e in existing]
+
+        goal = {}
+        for groups, qty in spec.iteritems():
+            for n in xrange(qty):
+                vm_name = '%s-%s-%d' % (self.name, groups, n)
+                goal[vm_name] = {'groups': groups}
+
+        new_vms = []
+        for vm_name, meta in goal.iteritems():
+            if vm_name not in existing_names:
+                meta = {'groups': meta['groups'], 'cluster': self.name}
+                flavor = {'ram': '2G'}
+                vm = cloud.boot(vm_name, apt_proxy=True, meta=meta,
+                                flavor=flavor, ping=False)
+                new_vms.append(vm)
+
+        for vm in existing:
+            if vm.name not in goal.keys():
+                print 'deleting %s' % vm
+                vm.delete()
+
+
+if __name__ == '__main__':
+    c = Cluster()
+    c.build('staging', {'apps': 2,
+                        'search,redis': 1,
+                        'db': 1,
+                        'cache': 1})
