e1856764ccb0f67fc530b06dc0515a0e55712cd2
Jesse Andrews <anotherjesse@gmail.com>
2012-11-17 14:39:31 -0800

    Ansible subclasses Cluster, add additional commands

--- intern/ansible.py
+++ intern/ansible.py
@@ -5,39 +5,38 @@ import intern.cloud
 import intern.cluster
 
 
-class Ansible(object):
+class Ansible(intern.cluster.Cluster):
 
-    def __init__(self, name, image=None, flavor=None, spec=None):
-        self.name = name
+    def __init__(self, name, image=None, flavor=None, spec=None, hostvars=None):
         self.spec = spec
         self.image = image
         self.flavor = flavor
-        self.cluster = intern.cluster.Cluster(name)
+        self.hostvars = hostvars
+        super(Ansible, self).__init__(name)
 
-    def list_json(self, build=True):
-        if build:
-            if self.cluster.build(self.spec):
-                self.cluster.wait_for_ssh()
+    def list_json(self):
 
         groups = {}
-        for vm in self.cluster:
+        for vm in self:
             for g in vm['groups'].split(','):
                 if g not in groups:
                     groups[g] = []
                 groups[g].append(vm.ip)
-        return json.dumps(groups)
+        return json.dumps(groups, sort_keys=True, indent=2)
 
     def host_json(self, ip):
-        for vm in self.cluster:
+        for vm in self:
             if vm.ip == ip:
                 info = {
                     'os:uuid': vm.id,
                     'os:ip': vm.ip,
                     'os:name': vm.name
                 }
+                if self.hostvars:
+                    info.update(self.hostvars)
                 for k, v in vm.server.metadata.iteritems():
                     info['os:meta:%s' % k] = v
-                return json.dumps(info)
+                return json.dumps(info, sort_keys=True, indent=2)
 
     def cli(self):
         if len(sys.argv) < 2:
@@ -50,4 +49,12 @@ class Ansible(object):
         elif cmd == '--host':
             ip = sys.argv[2]
             print self.host_json(ip)
+        elif cmd == '--kill':
+            # FIXME(ja): should probably wait until they disappear
+            for vm in self:
+                vm.delete()
+        elif cmd == '--build':
+            if self.build(self.spec):
+                self.wait_for_ssh()
+            print 'spec complete'
 
