00ab809381831376533c8751b12c52d8ec197fc0
Jesse Andrews <anotherjesse@gmail.com>
2012-11-17 14:19:22 -0800

    class based ansible inventory helper
    example ansible inventory script:
    
        import intern.ansible
        a = intern.ansible.Ansible('staging',
                                   image='precise',
                                   flavor={'ram': '2gb'},
                                   spec={
                                       'apps': 2,
                                       'cache': 1,
                                       'db': 1,
                                       'search': 1
                                   })
        a.cli()

--- intern/ansible.py
+++ intern/ansible.py
@@ -5,46 +5,49 @@ import intern.cloud
 import intern.cluster
 
 
-def cluster(name):
-    return intern.cluster.Cluster(name)
+class Ansible(object):
 
+    def __init__(self, name, image=None, flavor=None, spec=None):
+        self.name = name
+        self.spec = spec
+        self.image = image
+        self.flavor = flavor
+        self.cluster = intern.cluster.Cluster(name)
 
-def list_json(cluster, spec, build=True):
-    if build:
-        if cluster.build(spec):
-            cluster.wait_for_ssh()
-    groups = {}
-    for vm in cluster:
-        for g in vm['groups'].split(','):
-            if g not in groups:
-                groups[g] = []
-            groups[g].append(vm.ip)
-    return json.dumps(groups)
+    def list_json(self, build=True):
+        if build:
+            if self.cluster.build(self.spec):
+                self.cluster.wait_for_ssh()
 
+        groups = {}
+        for vm in self.cluster:
+            for g in vm['groups'].split(','):
+                if g not in groups:
+                    groups[g] = []
+                groups[g].append(vm.ip)
+        return json.dumps(groups)
 
-def host_json(cluster, ip):
-    for vm in cluster:
-        if vm.ip == ip:
-            info = {
-                'os:uuid': vm.id,
-                'os:ip': vm.ip,
-                'os:name': vm.name
-            }
-            for k, v in vm.server.metadata.iteritems():
-                info['os:%s' % k] = v
-            return json.dumps(info)
+    def host_json(self, ip):
+        for vm in self.cluster:
+            if vm.ip == ip:
+                info = {
+                    'os:uuid': vm.id,
+                    'os:ip': vm.ip,
+                    'os:name': vm.name
+                }
+                for k, v in vm.server.metadata.iteritems():
+                    info['os:meta:%s' % k] = v
+                return json.dumps(info)
 
+    def cli(self):
+        if len(sys.argv) < 2:
+            print 'usage: ansible-playbook -i %s (playbook)' % sys.argv[0]
+            sys.exit(1)
 
-def go(name, image, flavor, spec):
-    if len(sys.argv) < 2:
-        print 'usage: ansible-playbook -i %s (playbook)' % sys.argv[0]
-        sys.exit(1)
-
-    cmd = sys.argv[1]
-    cluster = intern.cluster.Cluster(name)
-    if cmd == '--list':
-        print list_json(cluster, spec)
-    elif cmd == '--host':
-        ip = sys.argv[2]
-        print host_json(cluster, ip)
+        cmd = sys.argv[1]
+        if cmd == '--list':
+            print self.list_json()
+        elif cmd == '--host':
+            ip = sys.argv[2]
+            print self.host_json(ip)
 
